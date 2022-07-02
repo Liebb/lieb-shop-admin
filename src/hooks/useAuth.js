a436cc85e04c4c4b6f1d50295f70b8b45cf1047f
Liebb <liebermorazan@gmail.com>
2022-07-01 23:37:14 -0600

    Creating a cookie to save the token and set an expiration time also added a custom error if the request is a 404

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -16,22 +16,26 @@ export const useAuth = () => {
 
 function useProvideAuth() {
   const [user, setUser] = useState(null);
+  const [error, setError] = useState();
 
   const signIn = async (email, password) => {
-    
     const options = {
-      headers : {
+      headers: {
         //Para que maneje de forma correcta todas la solicitudes
         accept: '*/*',
-        'Content-Type' : 'application/json',
-      }
+        'Content-Type': 'application/json',
+      },
     };
-    const { data: access_token } = await axios.post(endPoints.auth.login, {email, password}, options);
-    console.log(access_token);
+    const { data: access_token } = await axios.post(endPoints.auth.login, { email, password }, options);
+    if (access_token) {
+      Cookie.set('token', access_token.access_token, { expires: 5 });
+    }
   };
 
   return {
     user,
     signIn,
+    error, 
+    setError,
   };
 }
